24d9212ec8ee62535fc1e61d904aef486d1f3fe1
Semen Khizhnyak <gsem842@gmail.com>
2017-10-23 21:10:06 +0700

    add aliases Input, Html, View

--- config/app.php
+++ config/app.php
@@ -225,6 +225,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Input' => Illuminate\Support\Facades\Input::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Form' => Collective\Html\FormFacade::class,
 
     ],
 

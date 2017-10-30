946fec59edef65fa42aa03794edae0b1adbcce00
Semen Khizhnyak <gsem842@gmail.com>
2017-10-30 19:48:22 +0700

    add CRUD: MedService and MedSmena

---
+++ app/Http/Controllers/MedServiceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MedService;
+use Illuminate\Http\Request;
+
+class MedServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\MedService  $medService
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MedService $medService)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\MedService  $medService
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MedService $medService)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\MedService  $medService
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MedService $medService)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\MedService  $medService
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MedService $medService)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/MedSmenaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MedSmena;
+use Illuminate\Http\Request;
+
+class MedSmenaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\MedSmena  $medSmena
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MedSmena $medSmena)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\MedSmena  $medSmena
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MedSmena $medSmena)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\MedSmena  $medSmena
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MedSmena $medSmena)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\MedSmena  $medSmena
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MedSmena $medSmena)
+    {
+        //
+    }
+}
